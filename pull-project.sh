51adc842604581a0b62f4271e2150414d6a89ada
Andriy Guta <andyway@AWAir.local>
2015-07-23 10:16:40 +0300

    Added comments to pull script

--- pull-project.sh
+++ pull-project.sh
@@ -3,7 +3,7 @@
 export USER=alterwin
 export HOME=/home/alterwin
 
-REPO_DIR="~/files/$1"
+REPO_DIR="/files/$1"
 WEB_DIR="~/web/$2/public_html/"
 
 
@@ -13,20 +13,24 @@ check_run() {
 	echo "$changed_files" | grep --quiet "$1" && eval "$2"
 }
 
-if [ -d "$REPO_DIR" ]; then
+if [[  ! -d "${REPO_DIR}" || ! -L "${WEB_DIR}" ]]; then
+	echo "---- Entering directory $REPO_DIR ---"
 	cd $REPO_DIR
 
+	echo "---------------- Pulling from GitHub ---------------"
 	git pull
 
 	# Run `npm install` if package.json changed and `bower install` if `bower.json` changed.
 	check_run package.json "npm install"
 	check_run bower.json "bower install"
 
+	echo "-------------------- Building ----------------------"
 	gulp build
 
 #	rm -rf /home/alterwin/web/beta.alterhaus.com/public_html/*
 #	cp -R dist/* /home/alterwin/web/beta.alterhaus.com/public_html/
 
+	echo "--- Rsyncing from "$REPO_DIR/dist/" to $WEB_DIR ---"
 	rsync -rtv "$REPO_DIR/dist/" $WEB_DIR
 else
 	echo "Error: Directory $REPO_DIR does not exist."
